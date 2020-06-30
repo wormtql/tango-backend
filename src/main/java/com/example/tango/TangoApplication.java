8563f3f2474513c0cbb1ff6280df2943f7192226
IATOW <584130248@qq.com>
2020-06-30 22:04:11 +0800

    worm

---
+++ src/main/java/com/example/tango/TangoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tango/TangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
